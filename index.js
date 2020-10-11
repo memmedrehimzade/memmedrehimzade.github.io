7881551329690c23c31e50ff43f2f9595d3d34df
memmedrehimzade <memmed.rehimzade1998@gmail.com>
2020-10-11 22:21:26 +0400

    fix issue #1

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
-    $(".facebook").click(function(){
-        window.open( "https://facebook.com/memmed.r.rahimzade/","_blank")
-    }
-    )
-    $(".instagram").click(function(){
-        window.open ("https://instagram.com/memmed_rehimzade/", "_blank")
+    // $(".facebook").click(function(){
+    //     window.open( "https://facebook.com/memmed.r.rahimzade/","_blank")
+    // }
+    // )
+    // $(".instagram").click(function(){
+    //     window.open ("https://instagram.com/memmed_rehimzade/", "_blank")
 
-    })
-    $(".linkedin").click(function(){
-        window.open ("https://www.linkedin.com/in/m%C9%99mm%C9%99d-r%C9%99himzad%C9%99-0a8494163/", "_blank")
+    // })
+    // $(".linkedin").click(function(){
+    //     window.open ("https://www.linkedin.com/in/m%C9%99mm%C9%99d-r%C9%99himzad%C9%99-0a8494163/", "_blank")
 
-    })
+    // })
     
 }
 
